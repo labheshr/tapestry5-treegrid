854b3c5fd9b31bd5d615d77d64a87b530a93a0af
gabriel.landais <gabriel.landais@16786435-e542-0410-a2bd-ff856f703c97>
2008-01-23 19:23:03 +0000

    Children of a node is a list instead of an array Heartbeat removal Dual folding bug

--- tapestry5-treegrid/src/main/java/org/codelutin/tapestry/beans/TreeNode.java
+++ tapestry5-treegrid/src/main/java/org/codelutin/tapestry/beans/TreeNode.java
@@ -1,19 +1,23 @@
 package org.codelutin.tapestry.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeNode {
 
     public static final int TYPE_DOCUMENT = 0;
     public static final int TYPE_FOLDER = 1;
 
-    private TreeNode[] children;
+    private List<TreeNode> children;
     private String[] columns;
     private int type;
+    private Object object;
 
-    public TreeNode[] getChildren() {
+    public List<TreeNode> getChildren() {
         return children;
     }
 
-    public void setChildren(TreeNode[] children) {
+    public void setChildren(List<TreeNode> children) {
         this.children = children;
     }
 
@@ -31,6 +35,14 @@ public class TreeNode {
 
     public void setType(int type) {
         this.type = type;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
     }
 
 }
